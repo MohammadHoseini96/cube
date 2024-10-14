72fb5d55c4b647157b31fc005b8f19c378461639
Kian <mohammad.hoseini9675@gmail.com>
2024-10-14 14:49:44 +0330

    Feat. adding project's skeleton

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"cube/manager"
+	"cube/node"
+	"cube/task"
+	"cube/worker"
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"time"
+)
+
+//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+
+func main() {
+	t := task.Task{
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "Image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
+
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
+
+	w := worker.Worker{
+		Name:  "worker-1",
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
+
+	fmt.Printf("worker: %v\n", w)
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
+
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]*task.Task),
+		EventDb: make(map[string][]*task.TaskEvent),
+		Workers: []string{w.Name},
+	}
+
+	fmt.Printf("manager: %v\n", m)
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
+
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
+
+	fmt.Printf("node: %v\n", n)
+}
+
+//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
+// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
